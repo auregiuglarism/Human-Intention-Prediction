2260c5e3dbf2e83c9fd5351c3cf241147ccd2039
Aurelien Giuglaris-Michael <aurelien.giuglarism@gmail.com>
2023-11-01 20:06:05 +0100

    ZED Body tracking update
    Skeleton tracking first work with ZED 3D camera SDK

---
+++ ZED_body_tracking_group_10/reading_recording.py
@@ -0,0 +1,29 @@
+import cv2
+import sys
+import pyzed.sl as sl
+import ogl_viewer.viewer as gl
+import cv_viewer.tracking_viewer as cv_viewer
+import numpy as np
+import argparse
+
+zed = sl.Camera()
+
+# Set SVO path for playback
+input_path = sys.argv[1]
+init_parameters = sl.InitParameters()
+init_parameters.set_from_svo_file(input_path)
+
+# Open the ZED
+zed = sl.Camera()
+err = zed.open(init_parameters)
+
+svo_image = sl.Mat()
+while not exit_app:
+  if zed.grab() == sl.ERROR_CODE.SUCCESS:
+    # Read side by side frames stored in the SVO
+    zed.retrieve_image(svo_image, sl.VIEW.SIDE_BY_SIDE)
+    # Get frame count
+    svo_position = zed.get_svo_position();
+  elif zed.grab() == sl.ERROR_CODE.END_OF_SVOFILE_REACHED:
+    print("SVO end has been reached. Looping back to first frame")
+    zed.set_svo_position(0)
