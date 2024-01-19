a5c8b6d68800fc41af05b4431a9d44411f095a88
VitalijsTickovs <v.tickovs@student.maastrichtuniversity.nl>
2024-01-19 13:26:53 +0100

    probabilities can now be specified, saved to jsons and added to the graph configuration

--- ZED_body_tracking_group_10/configuration.py
+++ ZED_body_tracking_group_10/configuration.py
@@ -1,19 +1,26 @@
 import itertools
+import json
+import os
+from pathlib import Path
+
 import networkx as nx
 import matplotlib.pyplot as plt
+from probability_graph.worker_probs import worker_dir
 
 
 class Configuration:
     def __init__(self):
         self.G = None
+        self.worker_dir = worker_dir + "/worker_"
+
     def initGraph(self, input):
         G = nx.DiGraph()
         G.add_node("root", name=None, pos=None, prob=None)
         # append root to queue
         allPoss = list(itertools.permutations(input))
 
-        for pos in allPoss: # iterate through possibilities
-            prev_ref = "root" # keep track of the previous reference to add edges
+        for pos in allPoss:  # iterate through possibilities
+            prev_ref = "root"  # keep track of the previous reference to add edges
             for objTup in pos:
                 # get reference for this node
                 # ref = objTup[0]+str(objTup[1][0])+str(objTup[1][1])+str(objTup[1][2])+"_"+str(prev_ref)
@@ -27,28 +34,27 @@ class Configuration:
                         foundEqls = True
                         prev_ref = n
 
-
                 # Add all the new edges after iterating through the nodes
                 for edge in edges_to_add:
                     G.add_edge(*edge, weight=0.0)
                     # add node if not already added
                 if (not foundEqls and not G.has_node(ref)):
-                    G.add_node(ref, name=objTup[0], size = objTup[1])
+                    G.add_node(ref, name=objTup[0], size=objTup[1])
                     G.add_edge(prev_ref, ref, weight=0.0)
 
-                # add edge
+                    # add edge
                     prev_ref = ref
-                if(G.has_node(ref)):
+                if (G.has_node(ref)):
                     prev_ref = ref
 
         self.G = G
         return G
 
-    def checkEq(self,nd1, nd2):
+    def checkEq(self, nd1, nd2):
         splt1 = set(nd1.split("_"))
         splt2 = set(nd2.split("_"))
 
-        return (nd1!=nd2) and (splt1 == splt2)
+        return (nd1 != nd2) and (splt1 == splt2)
 
     def checkEq_baseline(self, nd1, nd2):
         splt1 = set(nd1.split("_"))
@@ -56,12 +62,46 @@ class Configuration:
 
         return (splt1 == splt2)
 
+    def get_non_pop_edges(self, edges):
+        non_pop = 0
+        prob_sum = 0
+        for u, v in edges:
+            edge_data = self.G.get_edge_data(u, v)
+            if edge_data["weight"] == 0:
+                non_pop += 1
+            else:
+                prob_sum += edge_data['weight']
+
+        prob_sum = 1 - prob_sum
+        return non_pop, prob_sum
+
     def assign_probs(self):
         G = self.G
         for node in G.nodes:
             edges = G.out_edges([node])
-            for edge in edges:
-                nx.set_edge_attributes(G, {edge:{"weight": round(1/len(edges), 3)}})
+            edge_num, prob_sum = self.get_non_pop_edges(edges)
+            for u,v in edges:
+                edge_data = G.get_edge_data(u, v)
+                if edge_data["weight"] == 0:
+                    G[u][v]["weight"] = round(prob_sum / edge_num, 3)
+
+
+    """
+        input - worker_id 
+        Assigns probs from json file in probability_graph/worker_probs
+        and then assigns all other values to be the same 
+        if we do not have information about other edges.
+    """
+    def assign_worker_probs(self, worker_id):
+        if worker_id is not None:
+            worker_file = Path(self.worker_dir + str(worker_id) + ".json")
+            if worker_file.is_file():
+                worker_probs = json.load(worker_file.open())
+                G = self.G
+                for used_node, edges in worker_probs.items():
+                    for edge in edges:
+                        G[used_node][edge[0]]["weight"] = edge[1]
+        self.assign_probs()
 
     def get_graph(self):
         return self.G
@@ -74,16 +114,17 @@ class Configuration:
 
 if __name__ == "__main__":
     configuration = [("Cup", (0), ("-1", "-1", "Crate0", "-1")),
-             ("Crate", (0), ("Cup0", "-1", "Cup1", "-1")),
-             ("Cup", (1), ("Crate0", "-1", "-1", "-1"))]
+                     ("Crate", (0), ("Cup0", "-1", "Cup1", "-1")),
+                     ("Cup", (1), ("Crate0", "-1", "-1", "-1"))]
 
     graph = Configuration()
     graph.initGraph(configuration)
-    graph.assign_probs()
+    graph.assign_worker_probs(1)
     # Draw the graph with edge labels
     pos = nx.spring_layout(graph.get_graph(), scale=3)
 
-    nx.draw(graph.get_graph(), pos, with_labels=True, font_size=10, font_color="black", font_weight="bold", arrowsize=20)
+    nx.draw(graph.get_graph(), pos, with_labels=True, font_size=10, font_color="black", font_weight="bold",
+            arrowsize=20)
 
     # Add edge labels
     labels = nx.get_edge_attributes(graph.get_graph(), 'weight')
